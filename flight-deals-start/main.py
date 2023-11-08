62e78f495225c7356d4a7a8150f5c2ff7e2805d1
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-11-08 08:53:43 -0300

    Feat: Showing cheap flights

--- flight-deals-start/main.py
+++ flight-deals-start/main.py
@@ -1,6 +1,12 @@
 from data_manager import DataManager
+import datetime
+from flight_search import FlightSearch
+
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
+
+ORIGIN_CITY_IATA = "LON"
 
 if sheet_data[0]["iataCode"] == "":
     from flight_search import FlightSearch
@@ -11,3 +17,14 @@ if sheet_data[0]["iataCode"] == "":
 
     data_manager.destination_data = sheet_data
     data_manager.update_destination_codes()
+
+tomorrow = datetime.datetime.now() + datetime.timedelta(days=1)
+six_month_from_today = datetime.datetime.now() + datetime.timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    flight = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
