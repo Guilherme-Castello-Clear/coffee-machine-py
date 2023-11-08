fe47aac1de73beedce97c723746b449f6bad9f57
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-11-08 08:43:41 -0300

    Feat: Reading and writing data

--- flight-deals-start/flight_search.py
+++ flight-deals-start/flight_search.py
@@ -1,3 +1,11 @@
+import requests
+
+TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
+TEQUILA_API_KEY = "-cb9qJBO3_8BqiESdaN-I9OF8lJgnSZc"
+
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+
+    def get_destination_code(self, city_name):
+        code = "test"
+        return code
