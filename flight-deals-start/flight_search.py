7cac6c23cbc170be62d45bc53c0cc1a3294999c6
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-11-08 08:48:47 -0300

    Feat: Searching cheap flights and saving on google sheet

--- flight-deals-start/flight_search.py
+++ flight-deals-start/flight_search.py
@@ -7,5 +7,10 @@ TEQUILA_API_KEY = "-cb9qJBO3_8BqiESdaN-I9OF8lJgnSZc"
 class FlightSearch:
 
     def get_destination_code(self, city_name):
-        code = "test"
+        location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {"term": city_name, "location_types": "city"}
+        response = requests.get(url=location_endpoint, headers=headers, params=query)
+        results = response.json()["locations"]
+        code = results[0]["code"]
         return code
